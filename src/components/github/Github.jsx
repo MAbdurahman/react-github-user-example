5fb9bca1eae0afcba8b4ac95d114541a1dc193d7
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-05 18:12:23 -0600

    Modify App.css; add Github.jsx and Github.css

---
+++ src/components/github/Github.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Github.css';
+import axios from 'axios';
+
+
+export default class Github extends Component {
+
+   constructor() {
+      super();
+   }
+
+
+   render() {
+      return (
+         <div className='github'>
+            <h1>Github</h1>
+         </div>
+      );
+   };
+};
